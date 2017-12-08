4ab5b671749e385ca4d6f9f432490aaafb6b2a0d
Dimitris Karakasilis <jimmykarily@gmail.com>
2017-12-08 17:04:02 +0200

    Write the needed artifacts to "output"

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,3 +6,7 @@ set -e -u
 
 cd java-buildpack-client-certificate-mapper
 ./mvnw -Dmaven.test.skip=true package
+
+mkdir -p ../output
+cp $(ls target/java-buildpack-client-certificate-mapper-*.jar | grep -v sources) ../output/
+echo $(cat target/maven-archiver/pom.properties  | grep version= | cut -d= -f2) > ../output/version
