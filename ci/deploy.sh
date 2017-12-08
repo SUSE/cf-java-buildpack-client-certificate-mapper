b5f7b2ba4832724f8c92a965244e7a3f92540957
Dimitris Karakasilis <jimmykarily@gmail.com>
2017-12-08 16:44:34 +0200

    Let there be output

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,4 +5,4 @@ set -e -u
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
 cd java-buildpack-client-certificate-mapper
-./mvnw -q -Dmaven.test.skip=true package
+./mvnw -Dmaven.test.skip=true package
